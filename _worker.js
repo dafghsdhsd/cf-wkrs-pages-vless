85b1d9fdc021e4ef4246377188e2cf56d6e14a02
dafghsdhsd <166990794+dafghsdhsd@users.noreply.github.com>
2025-05-02 16:24:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '22910ef7-3e93-4818-b282-6845c36dfd85';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
